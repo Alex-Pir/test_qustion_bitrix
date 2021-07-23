4922c2b6890dd1f6720ce998f291b3bac929928e
Александр Пирогов <a.pirogov@citrus-soft.ru>
2021-07-24 00:12:58 +0300

    Методы для работы с HL-блоком

--- local/components/artbyte/user.addresses/class.php
+++ local/components/artbyte/user.addresses/class.php
@@ -1,5 +1,7 @@
 <?php
 
+use Bitrix\Highloadblock\HighloadBlockTable;
+use Bitrix\Main\ArgumentOutOfRangeException;
 use Bitrix\Main\Loader;
 use Bitrix\Main\Localization\Loc;
 use Bitrix\Main\SystemException;
@@ -17,11 +19,19 @@ Loc::loadMessages(__FILE__);
  *
  * Class UserAddresses
  */
-class UserAddresses extends CBitrixComponent {
+class UserAddresses extends CBitrixComponent
+{
 
     /** @var string ID модуля Highload-блоки */
     const MODULE_ID_HL = "highloadblock";
 
+    /** @var string навзвание таблицы HL-блока */
+    const HL_TABLE_NAME = "artbyte_hl_user_addresses";
+
+    /** @var string поля HL-блока */
+    const USER_ID = "UF_USER_ID";
+    const ADDRESS = "UF_ADDRESS";
+    const ACTIVE = "UF_ACTIVE";
 
     /**
      * Выполнение компонента
@@ -40,10 +50,78 @@ class UserAddresses extends CBitrixComponent {
 
             $this->includeModule();
 
+
+            $this->initComponentTemplate();
         } catch (Exception $ex) {
             $this->abortResultCache();
             AddMessage2Log($ex->getMessage());
         }
+    }
+
+    /**
+     * Получение информации о HL-блоке
+     *
+     * @return array
+     * @throws SystemException
+     * @throws \Bitrix\Main\ArgumentException
+     * @throws \Bitrix\Main\ObjectPropertyException
+     */
+    protected function getHLTable(): array
+    {
+
+        $hlBlock = HighloadBlockTable::getList(
+            [
+                "filter" => ["=NAME" => self::HL_TABLE_NAME]
+            ]
+        )->fetch();
+
+        if (!$hlBlock) {
+            throw new SystemException(
+                Loc::getMessage(Loc::getMessage("UA_CLASS_HL_NOT_FOUND",
+                    ["#HL_NAME#" => self::HL_TABLE_NAME]))
+            );
+        }
+
+        return $hlBlock;
+    }
+
+    /**
+     * Получение данных из HL-блока
+     *
+     * @param array $hlBlock
+     * @param int $userId
+     * @param bool $active
+     * @return array
+     * @throws ArgumentOutOfRangeException
+     * @throws SystemException
+     * @throws \Bitrix\Main\ArgumentException
+     * @throws \Bitrix\Main\ObjectPropertyException
+     */
+    protected function getHLData(array $hlBlock, int $userId, bool $active = false): array
+    {
+
+        if ($userId <= 0)
+        {
+            throw new ArgumentOutOfRangeException(Loc::getMessage("UA_CLASS_USER_ID_ERROR"));
+        }
+
+        $filter = [
+            self::USER_ID => $userId
+        ];
+
+        if ($active)
+        {
+            $filter[self::ACTIVE] = "Y";
+        }
+
+        $hlDataClass = (HighloadBlockTable::compileEntity($hlBlock))->getDataClass();
+
+        return $hlDataClass::getList(
+            array(
+                "filter" => $filter,
+                "select" => [self::ADDRESS]
+            )
+        )->fetchAll();
     }
 
     /**
@@ -53,8 +131,10 @@ class UserAddresses extends CBitrixComponent {
      * @throws SystemException
      * @throws \Bitrix\Main\LoaderException
      */
-    protected function includeModule(): bool {
-        if (!Loader::includeModule(self::MODULE_ID_HL)) {
+    protected function includeModule(): bool
+    {
+        if (!Loader::includeModule(self::MODULE_ID_HL))
+        {
             throw new SystemException(Loc::getMessage("UA_CLASS_MODULE_NOT_INSTALLED",
                 ["#MODULE_ID#" => self::MODULE_ID_HL]
             ));
--- local/components/artbyte/user.addresses/lang/ru/class.php
+++ local/components/artbyte/user.addresses/lang/ru/class.php
@@ -1,2 +1,4 @@
 <?php
 $MESS["UA_CLASS_MODULE_NOT_INSTALLED"] = "Модуль #MODULE_ID# не установлен";
+$MESS["UA_CLASS_HL_NOT_FOUND"] = "Таблица #HL_NAME# не найдена";
+$MESS["UA_CLASS_USER_ID_ERROR"] = "ID пользователя должен быть больше 0";
